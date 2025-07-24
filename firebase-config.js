f1853483dc96b8d31f84e2158febe1414ed159bc
matesaman9910 <100013611+matesaman9910@users.noreply.github.com>
2025-07-24 16:35:29 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase config for VAF ID System
+const firebaseConfig = {
+  apiKey: "AIzaSyB9vkUmnwBE9Sy7e8U3FaNN_mta_AieC4g",
+  authDomain: "vaf-id-system.firebaseapp.com",
+  databaseURL: "https://vaf-id-system-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "vaf-id-system",
+  storageBucket: "vaf-id-system.firebasestorage.app",
+  messagingSenderId: "703691597265",
+  appId: "1:703691597265:web:aa6401b2e6288b2caf2cf0"
+};
+
+// Initialize Firebase (used in browser)
+firebase.initializeApp(firebaseConfig);
